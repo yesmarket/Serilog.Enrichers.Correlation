3b090b11bd5234da0384dcb70f8ea1d1ce2b841d
Ryan Bartsch <rbartsch@adam.com.au>
2020-08-26 10:22:35 +0930

    added ability to override header name

--- src/Serilog.Enrichers.Correlation/CorrelationIdObserver.cs
+++ src/Serilog.Enrichers.Correlation/CorrelationIdObserver.cs
@@ -10,8 +10,24 @@ namespace Serilog.Enrichers.Correlation
 {
    public sealed class CorrelationIdObserver : IObserver<DiagnosticListener>, IDisposable
    {
+      private readonly string _correlationIdName;
       private readonly List<IDisposable> _subscriptions = new List<IDisposable>();
       private bool _disposed;
+
+      public CorrelationIdObserver() : this("X-Correlation-ID")
+      {
+      }
+
+      public CorrelationIdObserver(string correlationIdName)
+      {
+         _correlationIdName = correlationIdName;
+      }
+
+      public void Dispose()
+      {
+         Dispose(true);
+         GC.SuppressFinalize(this);
+      }
 
       public void OnCompleted()
       {
@@ -33,7 +49,7 @@ namespace Serilog.Enrichers.Correlation
       public void OnHttpRequestInStart(HttpContext httpContext)
       {
          var headers = httpContext.Request.Headers;
-         if (headers.TryGetValue("X-Correlation-ID", out var header))
+         if (headers.TryGetValue(_correlationIdName, out var header))
             if (Guid.TryParse(header, out var correlationId))
             {
                Trace.CorrelationManager.ActivityId = correlationId;
@@ -42,17 +58,11 @@ namespace Serilog.Enrichers.Correlation
 
          Trace.CorrelationManager.ActivityId = Guid.NewGuid();
       }
-      
+
       [DiagnosticName("System.Net.Http.HttpRequestOut.Start")]
       public void OnHttpRequestOutStart(HttpRequestMessage request)
       {
-         request.Headers.Add("X-Correlation-ID", Trace.CorrelationManager.ActivityId.ToString());
-      }
-
-      public void Dispose()
-      {
-         Dispose(true);
-         GC.SuppressFinalize(this);
+         request.Headers.Add(_correlationIdName, Trace.CorrelationManager.ActivityId.ToString());
       }
 
       private void Dispose(bool disposing)
@@ -60,10 +70,7 @@ namespace Serilog.Enrichers.Correlation
          if (_disposed)
             return;
 
-         if (disposing)
-         {
-            _subscriptions.ForEach(_ => _.Dispose());
-         }
+         if (disposing) _subscriptions.ForEach(_ => _.Dispose());
 
          _disposed = true;
       }
